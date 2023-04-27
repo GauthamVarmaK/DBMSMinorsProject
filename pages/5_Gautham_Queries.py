bac96ccad3fcb19e036adee1aa5bf2a01b4f06ce
Gautham Varma K <k.gauthamvarma@gmail.com>
2023-04-27 21:53:09 +0530

    :wrench: Fix my 2 queries

--- pages/5_Gautham_Queries.py
+++ pages/5_Gautham_Queries.py
@@ -29,26 +29,27 @@ st.markdown('''
 ## Query 1
 ### Display the top 20 orders today sorted by price.
 ```sql
-SELECT * FROM orders ORDER BY price DESC LIMIT 20;
+SELECT orders.order_id,user_id,datetime,order_type,pm.amount FROM orders,payments as pm WHERE order_id=pm.order_id ORDER BY pm.amount DESC LIMIT 20;
 ```
 ''')
 
-cursor.execute("SELECT * FROM orders ORDER BY price DESC LIMIT 20")
+cursor.execute("SELECT orders.order_id,user_id,datetime,order_type,pm.amount FROM orders,payments as pm WHERE orders.order_id=pm.order_id ORDER BY pm.amount DESC LIMIT 20;")
 orders = cursor.fetchall()
 
-orders_df = pd.DataFrame(orders, columns=["order_id", "user_id", "comp_id", "quantity", "price", "order_date"])
+orders_df = pd.DataFrame(orders, columns=["order_id", "user_id", "datetime","order_type", "price"])
 st.dataframe(orders_df)
 
 st.markdown('''
 ## Query 2
 ### Find all resistors that are of 220 Ohms that are in Production
 ```sql
-SELECT * FROM components WHERE (comp_id IN (SELECT * from description WHERE param="resistance")) AND lifecycle = 'Production';
+SELECT * FROM components WHERE (comp_id IN (SELECT comp_id from description WHERE param='resistance' AND value='220k')) AND lifecycle = 'Production';
 ```
 ''')
 
-cursor.execute("SELECT * FROM components WHERE (comp_id IN (SELECT * from description WHERE param='resistance')) AND lifecycle = 'Production'")
+cursor.execute("SELECT * FROM components WHERE (comp_id IN (SELECT comp_id from description WHERE param='resistance' AND value='220k')) AND lifecycle = 'Production';")
 components = cursor.fetchall()
 
 components_df = pd.DataFrame(components, columns=["comp_id", "description", "part_no", "mf_id", "name", "lifecycle", "category", "datasheet", "rohs", "mount_type"])
 st.dataframe(components_df)
+
