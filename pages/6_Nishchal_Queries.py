de08827ac50bdf7cb7abb1c0a7b18373d5cdcbf0
NishchalAV <94071455+Limtree@users.noreply.github.com>
2023-05-02 12:25:15 +0530

    :ant: 5 Queries done and checked

--- pages/6_Nishchal_Queries.py
+++ pages/6_Nishchal_Queries.py
@@ -22,36 +22,36 @@ st.set_page_config(
 st.title("Nishchal Queries - [PES1UG21EC169]")
 
 st.markdown('''
-    This is the Queries page by Nishchal A V [PES1UG21EC69].
+    This is the Queries page by Nishchal A V [PES1UG21EC169].
 ''')
 
 st.markdown('''
 ## Query 1
-###  Find how many manufacturers are selling LEDS and have lifecycle as NFNPD.
+###  List the components that are limited in stock.
 ```sql
-SELECT components.comp_id,manufacturer.mf_id,components.lifecycle,components.name,components.category FROM components JOIN manufacturer ON manufacturer.name = components.name  AND components.lifecycle='NFNPD' AND (SELECT(components.category = 'LEDS'));
+SELECT stock.comp_id,components.part_no,stock.normally_stocking,stock.in_stock FROM stock JOIN components ON components.comp_id=stock.comp_id AND stock.in_stock < 100000 AND (SELECT(stock.normally_stocking = 'no'));"
 ```
 ''')
 
-cursor.execute("SELECT components.comp_id,manufacturer.mf_id,components.lifecycle,components.name,components.category FROM components JOIN manufacturer ON manufacturer.name = components.name  AND components.lifecycle='NFNPD' AND (SELECT(components.category = 'LEDS'));")
-components = cursor.fetchall()
+cursor.execute("SELECT stock.comp_id,components.part_no,stock.normally_stocking,stock.in_stock FROM stock JOIN components ON components.comp_id=stock.comp_id AND stock.in_stock < 100000 AND (SELECT(stock.normally_stocking = 'no'));")
+stock = cursor.fetchall()
 
-components_df = pd.DataFrame(components, columns=["comp_id", "mf_id", "lifecycle","name", "category"])
-st.dataframe(components_df)
+stock_df = pd.DataFrame(stock, columns=["comp_id", "part_no", "normally_stocking","in_stock"])
+st.dataframe(stock_df)
 
 st.markdown('''
 ## Query 2
-### List the components that are limited in stock
+### Tabulate orders with respect to user
 ```sql
-SELECT stock.comp_id,components.part_no,stock.normally_stocking FROM stock,components WHERE stock.in_stock < 100000 AND components.part_no = 'LTST-S270KGKT' AND stock.normally_stocking = 'no'
+SELECT users.user_id,orders.order_id,orders.date_time,orders.order_type FROM users,orders WHERE orders.user_id = users.user_id AND orders.date_time < '2022-07-01 00:00:00' AND order_type='Prime';
 ```
 ''')
 
-cursor.execute("SELECT stock.comp_id,components.part_no,stock.normally_stocking FROM stock,components WHERE stock.in_stock < 100000 AND components.part_no = 'LTST-S270KGKT' AND stock.normally_stocking = 'no'")
-stock = cursor.fetchall()
+cursor.execute("SELECT users.user_id,orders.order_id,orders.date_time,orders.order_type FROM users,orders WHERE orders.user_id = users.user_id AND orders.date_time < '2022-07-01 00:00:00' AND order_type='Prime';")
+users = cursor.fetchall()
 
-stock_df = pd.DataFrame(stock, columns=["comp_id", "part_no", "normally_stocking"])
-st.dataframe(stock_df)
+users_df = pd.DataFrame(users, columns=["user_id", "order_id", "date_time","order_type"])
+st.dataframe(users_df)
 
 st.markdown('''
 ## Query 3
@@ -72,11 +72,11 @@ st.markdown('''
 ## Query 4
 ### Check how many manufacturers are selling surface mount components with lifecyle of NFNPD and the components is resistor.
 ```sql
-SELECT components.mf_id,manufacturer.name,manufacturer.location,components.lifecycle,components.category FROM manufacturer JOIN components ON components.mf_id = manufacturer.mf_id AND (SELECT(mount_type='Surface Mount')) AND (SELECT(lifecycle='NFNPD')) AND components.category='Resistors';
+SELECT components.mf_id,manufacturer.name,manufacturer.location,components.lifecycle,components.category FROM manufacturer JOIN components ON components.mf_id = manufacturer.mf_id AND (SELECT(mount_type='Surface Mount')) AND (SELECT(lifecycle='InProduction')) AND components.category='Resistors';
 ```
 ''')
 
-cursor.execute("SELECT components.mf_id,manufacturer.name,manufacturer.location,components.lifecycle,components.category FROM manufacturer JOIN components ON components.mf_id = manufacturer.mf_id AND (SELECT(mount_type='Surface Mount')) AND (SELECT(lifecycle='NFNPD')) AND components.category='Resistors';")
+cursor.execute("SELECT components.mf_id,manufacturer.name,manufacturer.location,components.lifecycle,components.category FROM manufacturer JOIN components ON components.mf_id = manufacturer.mf_id AND (SELECT(mount_type='Surface Mount')) AND (SELECT(lifecycle='InProduction')) AND components.category='Resistors';")
 components = cursor.fetchall()
 
 components_df = pd.DataFrame(components, columns=["mf_id", "name", "location","lifecycle","category"])
@@ -96,4 +96,7 @@ payments = cursor.fetchall()
 
 payments_df = pd.DataFrame(payments, columns=["method", "order_id", "user_id","trnx_no","amount"])
 st.dataframe(payments_df)
+
+
+
 
